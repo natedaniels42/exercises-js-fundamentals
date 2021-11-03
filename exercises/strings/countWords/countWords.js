cf7dc5dede6e6d5ecd8e70a70f3ffc835b9a2fab
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-03 14:18:14 -0500

    Complete 3/3 from strings

--- exercises/strings/countWords/countWords.js
+++ exercises/strings/countWords/countWords.js
@@ -9,6 +9,8 @@
 
 function countWords(string) {
   // This is your job. :)
+
+  return string.split(" ").length;
 }
 
 if (require.main === module) {
@@ -16,6 +18,8 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(countWords('Running sanity checks for countWords:'));
 }
 
 module.exports = countWords;
