babbd7052738788e3b783fdea9f278b94ed81c43
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-03 13:56:26 -0500

    Factorial is complete

--- exercises/numbers/factorial/factorial.js
+++ exercises/numbers/factorial/factorial.js
@@ -22,8 +22,16 @@
  * @param {number} n - A non-negative integer `n`
  * @returns {number} The factorial of `n`
  */
-function factorial(n) {
-  // This is your job. :)
+const factorial = (n) => {
+  if (n < 0) {
+    return 'invalid';
+  }
+
+  if (n < 2) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 }
 
 if (require.main === module) {
@@ -31,6 +39,7 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  console.log(factorial(5));
 }
 
 module.exports = factorial;
