454df6558156517ba0dc94313400021c39386895
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-03 13:50:25 -0500

    Finished concat

--- exercises/arrays/concat/concat.js
+++ exercises/arrays/concat/concat.js
@@ -21,8 +21,8 @@
  * @param {object[]} rightArray - The right array to concatenate
  * @returns {object[]} The concatenation of the two given arrays
  */
-function concat(leftArray, rightArray) {
-  // This is your job. :)
+const concat = (left, right) => {
+  return left.concat(right);
 }
 
 if (require.main === module) {
@@ -31,5 +31,7 @@ if (require.main === module) {
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
+
+console.log(concat([1,2,3], [4,5,6]))
 
 module.exports = concat;
